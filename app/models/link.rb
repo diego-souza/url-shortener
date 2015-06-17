e9234bda4a5e1f8d053c9b239584a1f4784804b5
Diego Alves de Souza <diegosouza.mail@gmail.com>
2015-06-17 00:14:32 -0300

    Added a url normalizer method to add "http://" before urls that don't have it before saving in the database.

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,7 +1,9 @@
 class Link < ActiveRecord::Base
   validates :long_url, presence: true, uniqueness: true
+  validates_format_of :long_url, :with => /(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,6}(:[0-9]{1,5})?(\/.*‌​)?/ix
   validates :code, presence: true
   before_validation :generate_code, on: :create
+  before_validation :url_normalizer
 
   def generate_code
     code = random_string(8)
@@ -17,4 +19,7 @@ class Link < ActiveRecord::Base
     string
   end
 
+  def url_normalizer
+    self.long_url = "http://#{self.long_url}" unless self.long_url =~ /(http|https):\/\//ix
+  end
 end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,20 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Link, type: :model do
-  subject { Link.new(long_url: "www.google.com") }
+  subject { Link.new FactoryGirl.attributes_for(:link) }
 
-  it { should validate_presence_of(:long_url) }
-  it { should validate_uniqueness_of(:long_url) }
+  it { should allow_value('http://www.google.com',
+                          'http://www.google.com.br',
+                          'http://www.bbc.co.uk',
+                          'https://www.google.com',
+                          'https://mail.google.com').for(:long_url) }
+  it { should_not allow_value(nil,
+                              'www.google.com',
+                              'google',
+                              '1234567',
+                              'hHHp://www.bbc.co.uk',
+                              'http://www..google.c').for(:long_url) }
+
+  it "should validate uniqueness of long_url" do
+    subject.save!
+    should validate_uniqueness_of(:long_url)
+  end
 
   context "at creation" do
     it "should generate a code for a new url" do
       allow(subject).to receive(:random_string).and_return("ABC")
-      subject.save
+      subject.save!
       expect(subject.code).to eq("ABC")
     end
 
     it "should generate a unique code" do
-      second_subject = Link.new long_url: "www.yahoo.com.br"
+      second_subject = Link.new FactoryGirl.attributes_for(:link)
       allow(subject).to receive(:random_string).and_return("ABC")
       allow(second_subject).to receive(:random_string).and_return("ABC", "DEF")
 
@@ -23,5 +37,24 @@ RSpec.describe Link, type: :model do
       expect(subject.code).to eq("ABC")
       expect(second_subject.code).to eq("DEF")
     end
+
+    it "should add http:// to long_url if it doesn't have it already" do
+      subject.long_url = "www.google.com"
+      subject.save!
+      expect(subject.long_url).to eq("http://www.google.com")
+    end
+
+    it "should not change if it has http://" do
+      subject.long_url = "http://www.google.com"
+      subject.save!
+      expect(subject.long_url).to eq(subject.long_url)
+    end
+
+    it "should not change if it has https://" do
+      subject.long_url = "https://www.google.com"
+      subject.save!
+      expect(subject.long_url).to eq(subject.long_url)
+    end
+
   end
 end
